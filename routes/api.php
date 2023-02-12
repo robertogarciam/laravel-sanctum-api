8bbeda5a19f0427b453bdda799e7a02bcbafacff
Roberto <robertovgarciam@hotmail.com>
2023-02-11 21:49:34 -0500

    test

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
+//Route::post('/products', [ProductController::class, 'store']);
 
